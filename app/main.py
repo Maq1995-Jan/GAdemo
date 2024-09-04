4fffc50b6d75ed5d62e4c13899a98e3e5555e012
Mohamed Abuser Qurishe Ameenudeen <mameenudeen@pkrmsezdc.com>
2024-09-04 11:31:03 +0530

    Rerun

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/<randomString>')
 def returnBackwardsString(randomString):
-    #comment
+    
     return "".join(reversed(randomString))
 
 @app.route('/get-mode')
